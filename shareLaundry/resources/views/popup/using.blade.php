9523ecaeca7e7514b3c6c0a5f42aaa0398f6adcb
hyerimYeom <hyerim.yeom@kakao.com>
2022-02-10 20:08:57 +0900

    #6 모달 헤더에 기계 이름 가지고 오기
    * 처음에 laundry_id에 맞춰서 이름을 가지고 왔는데 당연히 machine이 가지고 있는 arr 순서 다름
      - machine을 foreach로 돌린 것의 id 중에서 laundry_id와 같은 걸 찾으니 맞는 것이 나옴
      - 항상 배열이 어떻게 되어있는지 확인하기

--- shareLaundry/resources/views/popup/using.blade.php
+++ shareLaundry/resources/views/popup/using.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">세탁기 이름 : </h5>
+                <h5 class="modal-title" id="machine_name"> </h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -12,7 +12,7 @@
                 <input type="hidden" name="laundry_id" value="">
                 <div class="input-group mb-3">
                     <span class="input-group-text">사용시간</span>
-                    <input type="text" class="form-control" aria-label="시간을 입력해주세요">
+                    <input type="text" class="form-control" aria-label="시간을 입력해주세요" name="duration_time" value="">
                     <span class="input-group-text">분</span>
                 </div>
             {{-- <a href="./using/" class="btn btn-success">사용하기</a> --}}
@@ -29,16 +29,24 @@
 <script>
     
     var myModal = document.getElementById('myModal');
-
+    // var machine ={{ json_encode($laundries) }};
+    var machines =<?= $laundries ?>;
     //여기서 초기화
     let laundry_id = 0;
-    let btnId = 0;
+    let machine_name = '';
     
     //누를때마다 확인하는건 추후 다른 방법 조사 후 수정
     document.addEventListener('click', function(e){
         if(e.target.id.includes('myInput')){
             laundry_id = e.target.dataset.indexNumber;
-            document.getElementsByName('laundry_id')[0].value =laundry_id
+            document.getElementsByName('laundry_id')[0].value =laundry_id;
         }         
+        
+        machines.forEach(machine => {
+            if(machine.id == laundry_id){
+                machine_name = machine.name;
+                document.getElementById('machine_name').innerHTML = machine_name;
+            }
+        });
     })
 </script>
